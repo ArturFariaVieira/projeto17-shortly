319a825aa682e48cb1484bb3e4f3c3f2fde5d586
Artur Faria Vieira <artur.f.vieira.94@gmail.com>
2022-12-24 00:05:43 -0300

    fix package pra deploy

--- src/server.js
+++ src/server.js
@@ -1,6 +1,8 @@
 import express from 'express';
 import router from './routes/indexRoutes.js';
 import cors from "cors";
+import dotenv from 'dotenv';
+dotenv.config();
 
 
 const app = express();
@@ -8,5 +10,5 @@ app.use(cors());
 app.use(express.json());
 app.use(router);
 
-const port = process.env.PORT || 5000;
+const port = process.env.PORT;
 app.listen(port, () => console.log(`Server running in port: ${port}`))
