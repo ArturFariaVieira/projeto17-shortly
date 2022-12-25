ce7ac53f26642295a0bc11ea2fdb8cff4937097d
Artur Faria Vieira <artur.f.vieira.94@gmail.com>
2022-12-25 02:11:52 -0300

    feat gets e delete url

--- src/controllers/urlsController.js
+++ src/controllers/urlsController.js
@@ -21,6 +21,59 @@ export async function shortenUrl(req, res){
 
 };
 
+export async function geturlbyId (req, res){
+    const {id} = req.params;
+    try{
+        const url = await connection.query(`SELECT links.id, links."shortUrl", url FROM links WHERE id = $1`, [id]);
+        if(url.rows.length < 1){
+            return res.sendStatus(404);
+        }
+        return res.send(url.rows[0]);
+    }catch(err){
+        console.log(err);
+        res.sendStatus(500);
+    }
+};
+
+export async function openurl (req, res){
+    const {shortUrl} = req.params;
+    try{
+        const url = await connection.query(`SELECT * FROM links WHERE "shortUrl" = $1`, [shortUrl]);
+        if(url.rows.length < 1){
+            return res.sendStatus(404);
+        }
+        const visit = await connection.query(`UPDATE links SET "visitCount" = links."visitCount" +1 WHERE id = $1;`, [url.rows[0].id])
+        res.status(301).redirect(url.rows[0].url);
+        
+    }catch (err){
+        console.log(err);
+        res.sendStatus(500);
+    }
+
+}
+
+export async function deleteUrl(req, res){
+    const {id} = req.params;
+    const user = res.locals.user;
+    console.log(user)
+
+    try{
+        const url = await connection.query(`SELECT * FROM links WHERE id = $1`, [id]);
+        if(url.rows.length < 1 ){
+            return res.sendStatus(404);
+        }
+        if(url.rows[0].userId != user.id){
+            console.log(url.rows[0].userId)
+            return res.sendStatus(401);
+        }
+        await connection.query(`DELETE FROM links WHERE id = $1`, [id])
+        res.sendStatus(204);
+    }catch(err){
+        console.log(err);
+        res.sendStatus(500);
+    }
+}
+
 
 
 
--- src/routes/urlsRouter.js
+++ src/routes/urlsRouter.js
@@ -1,13 +1,13 @@
 import { Router } from 'express';
 import schemasValidationMiddleware from '../middlewares/schemasMiddleware.js';
-import { shortenUrl } from '../controllers/urlsController.js';
+import { shortenUrl, geturlbyId, openurl, deleteUrl } from '../controllers/urlsController.js';
 import { tokenValidationMiddleware } from '../middlewares/tokenvalidationMiddleware.js';
 
 const urlsRouter = Router()
 urlsRouter.post("/urls/shorten", schemasValidationMiddleware, tokenValidationMiddleware, shortenUrl);
-//urlsRouter.get("/urls/:id", geturlbyId);
-//urlsRouter.get("/urls/open/:shortUrl", openurlbyId)
-//urlsRouter.delete("/urls/:id", deleteUrl)
+urlsRouter.get("/urls/:id", geturlbyId);
+urlsRouter.get("/urls/open/:shortUrl", openurl)
+urlsRouter.delete("/urls/:id", tokenValidationMiddleware, deleteUrl)
 
 export default urlsRouter;
 
